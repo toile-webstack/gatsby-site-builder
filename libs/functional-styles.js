3e32dc290427fdad9017bfe83fb9c080a950e9a8
MarcCoet <marcoet@gmail.com>
2019-10-16 14:27:22 +0200

    WIP: more functional styles

--- libs/functional-styles.js
+++ libs/functional-styles.js
@@ -39,4 +39,68 @@ export const backgroundImageWithCover = ({
     ...css,
   }
 
-export const dumb = {}
+// As seen on https://hankchizljaw.com/wrote/creating-an-aspect-ratio-css-utility/
+/* Needs to wrap the element like
+<div class="aspect-ratio-square">
+    <img src="" alt="" loading="lazy" />
+</div>
+*/
+export const aspectRatio = ({ ratio = 1 }) => {
+  return {
+    display: 'block',
+    position: 'relative',
+    paddingTop:
+      typeof ratio === 'string' && ratio.test(/%/)
+        ? ratio
+        : `${parseFloat(ratio) * 100}%`,
+    '& > *': {
+      display: 'block',
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+    },
+  }
+}
+
+// As seen on https://hankchizljaw.com/wrote/create-a-responsive-grid-layout-with-no-media-queries-using-css-grid/
+export const wrapper = ({ maxWidth = '65ch', space = '1rem' }) => ({
+  maxWidth,
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  padding: `0 ${space}`,
+})
+
+// As seen on https://hankchizljaw.com/wrote/create-a-responsive-grid-layout-with-no-media-queries-using-css-grid/
+/* Use it with the wrapper like
+<div class="wrapper">
+  <ul class="auto-grid">
+    <!-- items go here -->
+  </ul>
+</div>
+*/
+export const autoGrid = ({
+  maxWidthFallback = '400px',
+  min = '25ch',
+  space = '1rem',
+}) => ({
+  '& > *': {
+    maxWidth: maxWidthFallback,
+  },
+  '& > * + *': {
+    marginTop: space,
+  },
+
+  '@supports (display: grid)': {
+    display: 'grid',
+    gridTemplateColumns: `repeat(auto-fill, minmax(${min}, 1fr))`,
+    gridGap: space,
+    '& > *': {
+      maxWidth: 'unset',
+    },
+    '& > * + *': {
+      marginTop: 'unset',
+    },
+  },
+})
