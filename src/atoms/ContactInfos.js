82aa9cf916a7b248284da916440b22ebd9c30030
MarcCoet <marcoet@gmail.com>
2020-01-25 00:15:18 +0100

    FIX: footer contact info not rendering properly when out of SSR

--- src/atoms/ContactInfos.js
+++ src/atoms/ContactInfos.js
@@ -1,21 +1,26 @@
 import React from 'react'
 
-export default ({ name, email, phone }) =>
-  typeof window !== `undefined` ? (
-    <p
-      css={{
-        display: `flex`,
-        flexFlow: `row wrap`,
-        justifyContent: `center`,
-        '> *:not(:last-child):after': {
-          content: `"-"`,
-          margin: `0 3px`,
-          fontWeight: `normal`,
-        },
-      }}
-    >
-      {name && <span>{name}</span>}
-      {email}
-      {phone}
-    </p>
-  ) : null
+export default ({ name, email, phone }) => (
+  <p
+    css={{
+      display: `flex`,
+      flexFlow: `row wrap`,
+      justifyContent: `center`,
+      '> *:not(:last-child):after': {
+        content: `"-"`,
+        margin: `0 3px`,
+        fontWeight: `normal`,
+      },
+    }}
+  >
+    {typeof window !== `undefined` ? (
+      <>
+        {name && <span>{name}</span>}
+        {email}
+        {phone}
+      </>
+    ) : (
+      ''
+    )}
+  </p>
+)
